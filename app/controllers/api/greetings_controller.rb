871cda4c02fcf687f5c0133fbd605c43291625a7
Bondok6 <kirolousbondok6@gmail.com>
2022-05-30 19:19:05 +0200

    Install Frontend Dependencies

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::GreetingsController < ApplicationController
   def index
-    greetings = Greeting.find(Random.new.rand(1..5))
+    greetings = Greeting.find(Random.new.rand(1..10))
     render json: greetings
   end
 end 
