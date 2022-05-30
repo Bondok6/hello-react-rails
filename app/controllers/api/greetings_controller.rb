d4c8db028648780a6ef33c8a4d928a6814100ac4
Bondok6 <kirolousbondok6@gmail.com>
2022-05-31 00:54:10 +0200

    Random Greeting from 0 to 10

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -2,7 +2,7 @@ module Api
   # GreetingsController
   class GreetingsController < ApplicationController
     def index
-      greetings = Greeting.find(Random.new.rand(1..5))
+      greetings = Greeting.find(Random.new.rand(1..10))
       render json: greetings
     end
   end
