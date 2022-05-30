1bece12fafa84104b6506a0985ab9ff06e1b8c5e
Bondok6 <kirolousbondok6@gmail.com>
2022-05-30 19:07:27 +0200

    Create an API endpoint that selects a random greeting

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :greetings, only: [:index]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'static_pages#index'
 end
