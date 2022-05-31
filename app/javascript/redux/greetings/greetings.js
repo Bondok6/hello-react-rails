a1e0319f858f8fc7d4f1a3ba6ba3ce94fcfbf99d
Bondok6 <kirolousbondok6@gmail.com>
2022-05-31 02:03:52 +0200

    change the URL to /api/greetings

--- app/javascript/redux/greetings/greetings.js
+++ app/javascript/redux/greetings/greetings.js
@@ -10,11 +10,11 @@ const greetingReducer = (state = initState, action) => {
 };
 
 export const fetchgreeting = () => async (dispatch) => {
-  await fetch('http://127.0.0.1:3000/api/greetings')
+  await fetch('/api/greetings')
     .then((response) => response.json())
     .then((data) => {
       dispatch({ type: 'GET_GREETING', payload: data });
-    })
+    });
 };
 
 export default greetingReducer;
